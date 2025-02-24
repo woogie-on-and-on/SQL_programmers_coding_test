e125126ad79fc1dc9e057941fa376fe76e5ddbe2
woogie-on-and-on <dsgchoi17@gmail.com>
2025-02-24 17:55:58 +0900

    Update 경기도에 위치한 식품창고 목록 출력하기.sql

--- IS NULL/경기도에 위치한 식품창고 목록 출력하기.sql
+++ IS NULL/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -4,3 +4,13 @@ WHERE ADDRESS LIKE '경기도%'
 ORDER BY WAREHOUSE_ID ;
 -- LIKE : 특정 문자열 패턴을 '포함'하는 데이터를 찾을 때 사용
 -- IN : '명확'한 값의 목록을 찾을 때 사용 (%, _ 사용 불가) : 정확히 '경기도'만 검색이 가능
+
+-- ISNULL을 사용하는 경우
+SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS,
+      CASE
+      WHEN FREEZER_YN IS NULL THEN 'N'
+      ELSE FREEZER_YN
+      END AS FREEZER_YN
+FROM FOOD_WAREHOUSE
+WHERE ADDRESS LIKE '경기도%'
+ORDER BY WAREHOUSE_ID ;
